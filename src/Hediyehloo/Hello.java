d6d24891d30df08021c01a664c8ff84fdbf1480c
shediyehloo <shediyehloo@student.tgm.ac.at>
2018-01-30 11:47:03 +0100

    First Commit

---
+++ src/Hediyehloo/Hello.java
@@ -0,0 +1,12 @@
+package Hediyehloo;
+
+public class Hello {
+	public void hello() {
+		System.out.println("Hallo1");
+		
+	}
+	public void hello2() {
+		System.out.println("Hallo2");
+	}
+
+}
---
+++ src/Hediyehloo/main.java
@@ -0,0 +1,5 @@
+package Hediyehloo;
+
+public class main {
+
+}
